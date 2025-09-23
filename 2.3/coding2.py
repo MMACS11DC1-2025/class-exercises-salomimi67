52ec1d1187aaddf83d96940b63cbcedc3acfbe4b
salomimi67 <2036231@learn.vsb.bc.ca>
2025-09-23 22:06:56 +0000

    not done

--- 2.3/coding2.py
+++ 2.3/coding2.py
@@ -14,3 +14,23 @@ Judge 4: 8.5
 Judge 5: 9
 Your Olympic score is 8.0
 """
+print("What does judge 1 give out of 10?")
+judge_1 = float(input())
+
+print("What does judge 2 give out of 10?")
+judge_2 = float(input())
+
+print("What does judge 3 give out of 10?")
+judge_3 = float(input())
+
+print("What does judge 4 give out of 10?")
+judge_4 = float(input())
+      
+print("What does judge 5 give out of 10?")
+judge_5 = float(input())
+
+average = 0
+average = average + str(judge_1) + str(judge_2) + str(judge_3) + str(judge_4) + str(judge_5)
+final = 0
+final = final + average/5
+print("Your olympic score is " + str(final))
