389c74f62345f8878b826a46a79ebd9e2f32ee12
salomimi67 <2036231@learn.vsb.bc.ca>
2025-09-29 22:05:35 +0000

    changes

--- 2.5/comparing.py
+++ 2.5/comparing.py
@@ -6,3 +6,12 @@ You may use user input to add interactivity to the program.
 You must design your algorithm in English first, then translate it to Python code.
 Test as you go! Describe in your comments what steps you took to test your code.
 """
+# opens file
+file = open("reponses.csv")
+# deltes first line
+garbage = file.readline()
+
+ print("Please put in your name in to help find someone with the same similarities as you.")
+person = input()
+
+if 
