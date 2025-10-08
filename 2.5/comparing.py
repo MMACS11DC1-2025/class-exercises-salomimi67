cb7114494184e49f0c07359cb511a62166588ba0
salomimi <160702370+salomimi@users.noreply.github.com>
2025-10-08 21:11:51 +0000

    almost done

--- 2.5/comparing.py
+++ 2.5/comparing.py
@@ -19,11 +19,88 @@ person = input()
 # finds the person and their intrests
 for line in file:
     if person in line:
+        #turns their intests line into a list
         person_intrests = line.strip().split(",")
 
 most_common = ""
 most_point = 0
+other_intrests = ""
 
 for line in file:
-    if person in line:
-        person_intrests = line.strip().split(",")
+    point = 0
+    other_fav = line.strip().split(",")
+    other_person = other_intrests[1]
+
+    for intrest in person_intrests:
+        if intrest in person_intrests:
+            point += 0
+   # if person_intrests[2] == other_intrests[2]
+      #  point +=1
+   # if person_intrests[3] == other_intrests[3]
+       # point +=1
+   # if person_intrests[4] == other_intrests[4]
+        #point +=1
+   # if person_intrests[5] == other_intrests[5]
+        #point +=1
+   # if person_intrests[6] == other_intrests[6]
+        #point +=1
+    #if person_intrests[7] == other_intrests[7]
+        #point +=1
+   #if person_intrests[8] == other_intrests[8]
+        #point +=1
+    #if person_intrests[9] == other_intrests[9]
+        #point +=1
+   # other_person = other_intrests[1]
+
+    if point > most_point:
+        most_common = other_person
+        most_point = point
+print(most common " is the person with the most common similarities to you accordign to my data")
+if most_point == 8:
+    print("You guys have 8 similar intrests.")
+elif most_point <= 7 or most_point >= 6:
+    print("You guys have about 6-7 similar intrests.")
+elif most_point == 5:
+    print("You guys have 5 similar intrests.")
+elif most_point == 4:
+    print("You guys have 4 similar intrests.")
+elif most_point == 3:
+    print("You guys have 3 similar intrests.")
+elif most_point == 2:
+    print("You guys have 2 similar intrests.")
+elif most_point == 1:
+    print("You guys have 1 similar intrests.")   
+
+#if person_intrests[2] == other_intrests[2]
+    #print("You guys have the same favoutrite number!")
+    #if
+        #print("")
+#if person_intrests[3] == other_intrests[3]
+    #print("You guys have the same favoutrite pet!") 
+    #if
+        #print("")
+#if person_intrests[4] == other_intrests[4]
+    #print("You guys have the same favoutrite subject!") 
+    #if
+        #print("")
+#if person_intrests[5] == other_intrests[5]
+    #print("You guys have the same favoutrite sport (to play)!") 
+    #if
+        #print("")
+#if person_intrests[6] == other_intrests[6]
+    #print("You guys have the same favoutrite sport (to watch)!")
+    #if
+        #print("")
+#if person_intrests[7] == other_intrests[7]
+    #print("You guys have the same favoutrite genre of music!") 
+    #if
+        #print("")
+#if person_intrests[8] == other_intrests[8]
+    #print("You guys have the same favoutrite movie genre!") 
+    #if
+        #print("")
+#if person_intrests[9] == other_intrests[9]
+    #print("You guys have the same favoutrite fast food near by!") 
+    #if
+        #print("")
+        
