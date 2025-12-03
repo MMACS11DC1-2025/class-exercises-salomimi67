f460481ff985ed6f87391ec2c3b249fcd298d6ed
salomimi67 <2036231@learn.vsb.bc.ca>
2025-12-03 21:31:34 +0000

    doin

--- 6.2/search.py
+++ 6.2/search.py
@@ -12,6 +12,17 @@ for line in file:
 	if artist == "Drake":
 		drake_data.append([danceability, songtitle, artist])
 
-print("Dance score \tSong")
-for item in drake_data:
+for i in range(len(drake_data)):
+	small_score = drake_data[i]
+	small_index = i
+
+	for j range(i+1, len(drake_data)):
+		if drake_data[j] < small_score:
+		small_score = drake_data[y]
+		small_index = y
+	
+	drake_data[small_index], drake_data[i] = drake_data[i], drake_data{small_index}
+
+top_five = drake_data[:5]
+for item in top_five:
 	print(str(item[0]) + "\t\t" + item[1] + " by " + item[2])
